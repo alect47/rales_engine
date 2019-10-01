a22a738c7de159d91e811ed27ae504bdeb32e8f9
Alec Wells <alec.wells47@gmail.com>
2019-10-01 17:49:40 -0600

    Passin tests

--- app/controllers/api/v1/customers/search_controller.rb
+++ app/controllers/api/v1/customers/search_controller.rb
@@ -1,8 +1,18 @@
 class Api::V1::Customers::SearchController < ApplicationController
+
   def index
+    render json: CustomerSerializer.new(Customer.where(customer_params))
+
   end
 
   def show
+    render json: CustomerSerializer.new(Customer.find_by(customer_params))
     # binding.pry
+  end
+
+private
+
+  def customer_params
+    params.permit(:first_name, :last_name, :id, :created_at, :updated_at)
   end
 end
