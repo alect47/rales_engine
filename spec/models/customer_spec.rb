6998dc565bd07f05642b45ff2679517a099a09e0
Alec Wells <alec.wells47@gmail.com>
2019-10-01 13:50:48 -0600

    Finish model tests

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -5,4 +5,11 @@ RSpec.describe Customer, type: :model do
     it { should validate_presence_of :first_name}
     it { should validate_presence_of :last_name}
   end
+
+  describe 'relationships' do
+    it { should have_many :invoices}
+    it { should have_many(:invoice_items).through(:invoices)}
+    it { should have_many(:transactions).through(:invoices)}
+    it { should have_many(:items).through(:invoice_items)}
+  end
 end
