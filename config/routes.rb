282a542006d1b0b604c84cd0ddb033e18b4d38e3
Alec Wells <alec.wells47@gmail.com>
2019-10-02 06:49:55 -0600

    Fix invoice request spec and delete extra routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,4 @@
 Rails.application.routes.draw do
-
-  # namespace :api do
-  #   namespace :v1 do
-  #     namespace :customers do
-  #       get 'search/index'
-  #     end
-  #   end
-  # end
-  #
-  # namespace :api do
-  #   namespace :v1 do
-  #     namespace :customers do
-  #       get 'search/show'
-  #     end
-  #   end
-  # end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
@@ -23,6 +7,7 @@ Rails.application.routes.draw do
         get '/find_all', to: 'search#index'
       end
       resources :customers, only: [:index, :show]
+      resources :invoices, only: [:index, :show]
     end
   end
 end
