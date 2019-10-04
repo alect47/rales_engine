3c50f11ffb3d88056500001cfb5d6fda74ad3f7d
Alec Wells <alec.wells47@gmail.com>
2019-10-04 07:49:47 -0600

    Add relationship for invoice and customer

--- spec/requests/api/v1/invoices/invoice_items_request_spec.rb
+++ spec/requests/api/v1/invoices/invoice_items_request_spec.rb
@@ -36,4 +36,22 @@ describe "Invoice invoice_items API" do
     items = JSON.parse(response.body)
     expect(items["data"].count).to eq(1)
   end
+
+  it "sends customer associated with invoice" do
+    merchant = create(:merchant)
+    item = create(:item, merchant: merchant)
+    customer = create(:customer)
+    invoice_1 = create(:invoice, merchant: merchant, customer: customer)
+    invoice_1_id = invoice_1.id
+    transaction_1 = create(:transaction, invoice: invoice_1)
+    transaction_2 = create(:transaction, invoice: invoice_1)
+    invoice_item_1 = create(:invoice_item, item: item, invoice: invoice_1)
+
+    get "/api/v1/invoices/#{invoice_1_id}/customer"
+
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body)
+    expect(items["data"]["attributes"]["id"]).to eq(customer.id)
+  end
 end
