4e0c036844490e74aadec3858c791026407cb626
Alec Wells <alec.wells47@gmail.com>
2019-10-04 20:16:01 -0600

    Cleanup invoice_items controller

--- app/controllers/api/v1/invoice_items/items_controller.rb
+++ app/controllers/api/v1/invoice_items/items_controller.rb
@@ -1,14 +1,12 @@
 class Api::V1::InvoiceItems::ItemsController < ApplicationController
 
   def show
-    # binding.pry
     render json: ItemSerializer.new(Item.order_items.find_by_invoiceitem(item_params[:invoice_item_id]))
   end
 
 private
 
   def item_params
-    # binding.pry
     params.permit(:invoice_item_id)
   end
 end
