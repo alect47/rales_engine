54c4abcb4a83faaeaba1be5aa293eed335b8a760
Alec Wells <alec.wells47@gmail.com>
2019-10-06 16:54:07 -0600

    Update readme

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
         get '/find_all', to: 'search#index'
         get '/:customer_id/invoices', to: 'invoices#index'
         get '/:customer_id/transactions', to: 'transactions#index'
+        get '/:customer_id/favorite_merchant', to: 'favorite_merchant#show'
       end
       namespace :items do
         get '/find', to: 'search#show'
